16f73d7323f2498b15ac4208d8902058a16df10d
Sven Kubiak <sk@svenkubiak.de>
2021-04-14 13:35:18 +0200

    Version bumps

--- src/test/java/tests/hibernate/DataStoreTest.java
+++ src/test/java/tests/hibernate/DataStoreTest.java
@@ -1,8 +1,8 @@
 package tests.hibernate;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Optional;
